90b83a1127ecdb03c6daa288e7d1d3420e658b54
Max Khapaev <36515712+Khap314@users.noreply.github.com>
2026-06-16 18:40:30 +0300

    Create database.py

---
+++ utility/database.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from sqlalchemy import create_engine
+import pandas as pd
+import streamlit as st
+
+@st.cache_data
+def get_data_from_db():
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    db_path = BASE_DIR/"data"/"quiz.db"
+    
+    if not db_path.is_file():
+        return "NO_FILE", None
+        
+    engine = create_engine(f"sqlite:///{db_path.as_posix()}")
+    df = pd.read_sql_query("SELECT * FROM games ORDER BY date(date)", con=engine)
+    
+    if df.empty:
+        return "EMPTY_DB", None
+        
+    df['date'] = pd.to_datetime(df['date'])
+    df['day_num'] = df['date'].dt.dayofweek
+    ru_days = {0: 'Понедельник', 1: 'Вторник', 2: 'Среда', 3: 'Четверг', 4: 'Пятница', 5: 'Суббота', 6: 'Воскресенье'}
+    df['day_name'] = df['day_num'].map(ru_days)
+    return "OK", df
